4754e0ef9d4772420c46e216ff70ddd0833f8343
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2025-01-30 00:34:51 +0530

    backed part

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-require('dotenv').config()
-const express=require("express")
-const connection=require("./config/db")
-const { userRoute } = require('./routes/UserRoute')
-const { ProductRoute } = require('./routes/ProductRoute')
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const connection = require("./config/db");
+const { userRoute } = require("./routes/UserRoute");
+const { ProductRoute } = require("./routes/ProductRoute");
+const cors = require("cors");
 
-const app=express()
+const app = express();
 
-app.use(cors(
-    {
-        origin: 'http://localhost:5177/',
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-         methods: "GET,POST,PUT,DELETE"
-    }
-))
-app.use(express.json())
-app.use("/user",userRoute)
-app.use("/Product",ProductRoute)
+app.use(
+  cors({
+    origin: "http://localhost:5177",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: ["GET,POST,PUT,DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use("/user", userRoute);
+app.use("/Product", ProductRoute);
 
-const PORT= 8000
-app.listen(PORT,async()=>{
-    try {
-        await connection();
-        console.log("server running successfully")
-    } catch (error) {
-        console.log({error:error.message})
-    }
-
-})
+const PORT = 8000;
+app.listen(PORT, async () => {
+  try {
+    await connection();
+    console.log("server running successfully");
+  } catch (error) {
+    console.log({ error: error.message });
+  }
+});
