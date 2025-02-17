7ce0597af71c4f6e7051c4642a7121b9cf75a50d
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2025-02-18 00:14:31 +0530

    ..

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ const cors = require("cors");
 const app = express();
 
 // Enable CORS with correct settings
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173", // Allow frontend
-//     methods: ["GET", "POST", "PUT", "DELETE"], // Fix method format
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Fix method format
+    credentials: true,
+  })
+);
 
 // Handle preflight requests
 app.options("*", cors());
